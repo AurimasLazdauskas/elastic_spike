e973d0252e86c50074dd10b71292fde904f44908
AurimasLazdauskas <aur.lazdauskas@gmail.com>
2018-06-25 16:15:47 +0300

    Query accounts by name using ES

--- app/services/elastic/query.rb
+++ app/services/elastic/query.rb
@@ -4,6 +4,13 @@ module Elastic
       @client = Elasticsearch::Client.new log: true
     end
 
+    def self.by_name(term)
+      new.query(term)
+    end
 
+    def query(term)
+      result = @client.search index: 'accounts', body: { query: { match: { name: term } } }
+      result['hits']['hits'].map { |h| h['_source']['name'] }
+    end
   end
 end
